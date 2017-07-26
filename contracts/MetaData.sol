46127f70031fe0877b32d782155e59a5fe13f81f
Christos Nasikas <xristosnasikas>
2017-07-26 19:58:22 +0300

    Rename Data to Metadata and change message type

---
+++ contracts/MetaData.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.2;
+
+/**
+ * Contract responsible for handling the metaData
+ * In the future will be hash pointers to a db
+ */
+
+contract MetaData {
+    
+    address public owner;
+    
+    bytes32 message;
+
+	function MetaData (bytes32 _message) {
+		owner = msg.sender;
+		message = _message;
+	}
+	
+	function get() constant returns (uint[]) {
+        return message;
+    }
+}
