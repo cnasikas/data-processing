749505d5fa948c87c0423a9f6ec4fea9bd5f5561
Christos Nasikas <xristosnasikas>
2017-07-26 17:41:39 +0300

    Add bootstrap nav html

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-class AppNav extends Component {
 
-	constructor(props) {
-    	super(props);
-  	}
+export default class Nav extends React.Component {
 
-	render() {
-    	return (
-      		<nav></nav>
-    	);
-  	}
+    render() {
+
+	    const menuItems = this.props.menuItems.map((item) =>
+		  <NavLink className="navbar-brand" key={item.id} to={item.url}>{item.title}</NavLink>
+		);
+
+        return(
+
+        	<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+			  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			    <span className="navbar-toggler-icon"></span>
+			  </button>
+			  <NavLink to="/" className="navbar-brand">{this.props.title}</NavLink>
+			  <div className="collapse navbar-collapse" id="navbarNav">
+			    <ul className="navbar-nav">
+			      {menuItems}
+			    </ul>
+			  </div>
+			</nav>
+        );
+    }
 }
-
-export default AppNav;
