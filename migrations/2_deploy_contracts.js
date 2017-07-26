e98a9a29c00b79b60c42b427b3df9daf5e1c0811
Christos Nasikas <xristosnasikas>
2017-07-26 20:20:07 +0300

    Add truffle-contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-//ar ConvertLib = artifacts.require("./ConvertLib.sol");
-//var MetaCoin = artifacts.require("./MetaCoin.sol");
+var MetaData = artifacts.require("../contracts/MetaData.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(ConvertLib);
-  //deployer.link(ConvertLib, MetaCoin);
-  //deployer.deploy(MetaCoin);
+  deployer.deploy(MetaData);
 };
