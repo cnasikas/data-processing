0d6f955bc82ac289524c44012775cc20cc624cb2
Christos Nasikas <xristosnasikas>
2017-07-26 17:34:05 +0300

    Add main page to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AppHeader from './components/Header'
+import AppMain from './components/Main'
 import AppFooter from './components/Footer'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="app">
+        <div className="app-wrapper">
           <AppHeader className="app-header"></AppHeader>
+          <AppMain className="app-main"></AppMain>
           <AppFooter className="app-footer"></AppFooter>
         </div>
-      </MuiThemeProvider>
     );
   }
 }
-
-export default App;
