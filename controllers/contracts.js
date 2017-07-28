3ad0771ce7c87d537738bfcc2479ee3065e04db9
Christos Nasikas <xristosnasikas>
2017-07-28 15:15:28 +0300

    Add post route

--- controllers/contracts.js
+++ controllers/contracts.js
@@ -1,13 +1,32 @@
 import { Router } from 'express'
+import contracts from '../services/Contracts.js'
+import node from '../services/Node.js'
 
 export default ({config}) => {
     
     let router = Router();
 
-	/* Routes */
+	/* Contract Routes */
 
 	router.get('/', (req, res) => {
-  		res.json({msg: 'Hello contract!'})
+
+		//let response = Object.assign({}, ...Object.keys(contracts).map(k => ({ [k]:  })));
+
+		//const {contract, ...result } = contracts;
+
+  		res.json({contracts: contracts})
+
+	}).post('/', (req, res) => {
+
+		let bytecode = contracts.metadata.unlinked_binary;
+		let gasEstimate = node.getLibInstance().eth.estimateGas({data: bytecode}) + 20000
+
+		contracts.metadata.new('message!', {gas: gasEstimate}).then((instance) => {
+			res.json({msg: 'new contract succesfully created'})
+		})
+		.catch(function(err) {
+  			res.json({error: err.message})
+		})
 	})
 
 	return router
