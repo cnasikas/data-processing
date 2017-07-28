351764948f1b1fbdb321ed2e16bc85ba945b2de2
Christos Nasikas <xristosnasikas>
2017-07-28 15:10:48 +0300

    Ajax call to /contracts, add contract template

--- src/pages/Contracts.js
+++ src/pages/Contracts.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
+
+import Contract from '../components/Contract.js'
+
+import '../css/Contracts.css'
 
 export default class Contracts extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			contracts: null
+		}
+	}
+
+	componentDidMount(){
+
+		axios.get('http://localhost:3001/api/contracts').then( (response) => {
+
+			this.setState((prevState, props) => {
+			  return {contracts: response.data.contracts};
+			})
+
+		}).catch( (error) => {
+   			console.log(error)
+  		});
+	}
+
     render() {
+
+    	let contracts = ''
+
+    	if(this.state.contracts){
+
+    		contracts = this.state.contracts.map( (contract) => {
+    			return <Contract key={contract.id} title={contract.title} desc={contract.desc} id={contract.id}></Contract>
+    		})
+
+    	}
+
         return(
-            <div>Yo contracts!</div>
+
+            <section id="contracts">
+            	{contracts}	
+            </section>
         );
     }
 }
