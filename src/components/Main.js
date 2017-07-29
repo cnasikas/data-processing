70c23db7f8daf3b54010f67c24ada28748e93e22
Christos Nasikas <xristosnasikas>
2017-07-29 21:14:35 +0300

    Add login route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Login from './Login.js'
+
 import Home from '../pages/Home.js'
 import Contracts from '../pages/Contracts.js'
 import Requests from '../pages/Requests.js'
@@ -14,6 +16,7 @@ export default class Main extends React.Component {
               <Route exact path='/' component={Home}/>
               <Route path='/contracts' component={Contracts}/>
               <Route path='/requests' component={Requests}/>
+              <Route path='/login' component={Login}/>
           </Switch>
         </main> 
     	)
