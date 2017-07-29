20a24a047ed4ecd796483be75374d4bf61314b9f
Christos Nasikas <xristosnasikas>
2017-07-29 21:23:04 +0300

    Init on server load

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import init from './services/Init.js'
+
 import nodeMiddleware from './middlewares/NodeMiddleware.js'
 
 import controllers from './controllers/controllers.js'
@@ -30,6 +32,8 @@ app.use(bodyParser.json({
 app.use(nodeMiddleware())
 
 app.use('/api', controllers({config}))
+
+init()
 
 app.listen(process.env.PORT || config.port, function () {
 	console.log(`Started on port ${this.address().port}`);
