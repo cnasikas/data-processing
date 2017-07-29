73237494ca5490b7ead7282d0012a24bd5ae4bce
Christos Nasikas <xristosnasikas>
2017-07-29 21:19:34 +0300

    Add init service

---
+++ services/Init.js
@@ -0,0 +1,20 @@
+import low from 'lowdb'
+import _ from 'lodash'
+
+function db() {
+
+	/* Write returns a promise. Fix it. */
+	/* Start server listen afterd db init and hanlde errors */
+
+	const contracts = low('./db/contracts.js')
+	
+    if(_.isEmpty(contracts.getState())){
+
+    	contracts.defaults({ contracts: [], user: {} }).write()
+    }
+}
+
+export default () => {
+
+	db()
+}
