460aa5422f01acb6e1d2e070c74bc8c1983d1cf2
Christos Nasikas <xristosnasikas>
2017-07-29 21:23:57 +0300

    Change contracts structure

--- services/Contracts.js
+++ services/Contracts.js
@@ -2,23 +2,23 @@ import contract from 'truffle-contract'
 import MetaDataArtifact from '../build/contracts/MetaData.json'
 import node from '../services/Node.js'
 
-let contracts = [
-	{	
+let contracts = {
+	metadata: {	
 		id: 'metadata',
 		title: 'MetaData',
 		desc: 'In this type of contract you can put an encrypted hash pointer (metadata) pointing to the data',
 		contract: contract(MetaDataArtifact)
 	},
-	{	
+	request: {	
 		id: 'request',
 		title: 'Request',
 		desc: 'In this type of contract you can request for a computation over some data',
 		contract: contract(MetaDataArtifact)
 	}
-]
+}
 
-for (let contract of contracts){
-	contract.contract.setProvider(node.getProvider())
+for (let key of Object.keys(contracts)) {
+    contracts[key].contract.setProvider(node.getProvider())
 }
 
 export default contracts
