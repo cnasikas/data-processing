878d36f1b457ea81fb39afea2cd9798588d74168
Christos Nasikas <xristosnasikas>
2017-08-04 01:10:51 +0300

    Change contract html output

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 export default class Contract extends React.Component {
 	
     render() {
         return(
-            <div className="contract card text-center">
+            <div className="contract card">
+                <div className="card-header">
+                    Address: {this.props.id}
+                </div>
             	<div className="card-block">
-            		<h4 className="card-title">{this.props.id}</h4>
+            		<h4 className="card-title">Info</h4>
+                     <h6 className="card-subtitle text-muted">Tx Hash: {this.props.hash}</h6>
             	</div>
+                <div className="card-footer text-muted">
+                    Created at: {this.props.date}
+                </div>
             </div>
         );
     }
