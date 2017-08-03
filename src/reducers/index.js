6e10d6e1340cf48f6f7ed10972c96e18ae6cc911
Christos Nasikas <xristosnasikas>
2017-08-04 01:16:57 +0300

    Add contracts, reduxForm to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
-import { combineReducers } from "redux";
-import notifications from "./notifications";
+import { combineReducers } from "redux"
+import { reducer as formReducer } from 'redux-form'
+import notifications from "./notifications"
+import contracts from "./contracts"
 
 export default combineReducers({
-  notifications
+  notifications,
+  contracts,
+  form: formReducer
 });
