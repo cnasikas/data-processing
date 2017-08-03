5058b7c5f56a99afa107f2b321064f4fd23087a3
Christos Nasikas <xristosnasikas>
2017-08-04 01:19:38 +0300

    Change contract types component logic to support redux

--- src/components/ContractTypes.js
+++ src/components/ContractTypes.js
@@ -1,50 +1,41 @@
 import React from 'react'
-import axios from 'axios'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
 
+import { getContractTypes } from "../actions/ActionCreators";
 import ContractType from '../components/ContractType.js'
 
 import '../css/Contracts.css'
 
-export default class ContractTypes extends React.Component {
+class ContractTypes extends React.Component {
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			contracts: null
-		}
-	}
-
-	componentDidMount(){
-
-		axios.get('http://localhost:3001/api/contracts/types').then( (response) => {
-
-			this.setState((prevState, props) => {
-			  return {contracts: response.data.contracts};
-			})
-
-		}).catch( (error) => {
-   			console.log(error)
-  		});
-	}
+    componentDidMount(){
+        this.props.actions.getContractTypes()
+    }
 
     render() {
 
-    	let contracts = ''
-
-    	if(this.state.contracts){
-
-            contracts = [...Object.keys(this.state.contracts)].map((key) => {
-                let contract = this.state.contracts[key]
-                return <ContractType key={contract.id} title={contract.title} desc={contract.desc} id={contract.id}></ContractType>
-            })
-
-    	}
+        let types = ''
+        
+        if(this.props.types.length > 0){
+            types = this.props.types.map ((type, index) => <ContractType key={index} {...type}></ContractType>)
+        }
 
         return(
 
-            <section id="contracts">
-            	{contracts}	
+            <section id="contract-types">
+                {types} 
             </section>
         );
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ getContractTypes }, dispatch)
+});
+
+const mapStateToProps = state => ({
+  types: state.contracts
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContractTypes);
