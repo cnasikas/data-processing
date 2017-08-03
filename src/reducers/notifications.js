4c977b76060bcc25a8d881730358586cd5e69321
Christos Nasikas <xristosnasikas>
2017-08-04 01:15:48 +0300

    Add ADD_NOTIFICATION action to reducer

--- src/reducers/notifications.js
+++ src/reducers/notifications.js
@@ -1,10 +1,13 @@
-import { ADD_TO_CART } from '../constants/ActionTypes'
+import { ADD_NOTIFICATION } from '../actions/ActionTypes'
 
 export default function notifications(state = [], action) {
 	
   const { payload, type } = action;
 
   switch (type) {
+
+  	case ADD_NOTIFICATION:
+  		return [payload, ...state]
 
     default:
       return state;
