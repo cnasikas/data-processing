d6ba35d2b4d950ad8a676e5936dc66172572b621
Christos Nasikas <xristosnasikas>
2017-08-23 13:57:18 +0300

    Change contract details html markup

--- src/components/ContractDetails.js
+++ src/components/ContractDetails.js
@@ -19,17 +19,17 @@ class ContractDetails extends React.Component {
             return (<div></div>)
 
         return(
-            <article className="contract-details">
-                <div className="contract-header">
-                  <h1>Contract: {this.props.contract[0].id}</h1>
-                </div>
-                <div className="contract-body">
-                    <ul className="list-group">
-                      <li className="list-group-item">Hash: {this.props.contract[0].hash}</li>
-                      <li className="list-group-item">Owner: {this.props.contract[0].owner}</li>
-                    </ul>
-                </div>
-            </article>
+          <article className="card border-dark text-dark contract-details">
+            <div className="card-header contract-header">
+              Conctract Details
+            </div>
+            <div className="card-body">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">TxHash: {this.props.contract[0].hash}</li>
+                <li className="list-group-item">Owner: {this.props.contract[0].owner}</li>
+              </ul>
+            </div>
+          </article>
         );
     }
 }
