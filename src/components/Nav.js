894b6816931d07769ad450e4861bb638209e234a
Christos Nasikas <xristosnasikas>
2017-08-23 12:04:49 +0300

    Fix navigation html markup

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,21 +7,21 @@ export default class Nav extends React.Component {
     render() {
 
 	    const menuItems = this.props.menuItems.map((item) =>
-		  <NavLink className="navbar-brand" key={item.id} to={item.url}>{item.title}</NavLink>
+		  <li className="nav-item"><NavLink className="nav-link" key={item.id} to={item.url}>{item.title}</NavLink></li>
 		);
 
         return(
 
-        	<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-			  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			    <span className="navbar-toggler-icon"></span>
-			  </button>
-			  <NavLink to="/" className="navbar-brand">{this.props.title}</NavLink>
-			  <div className="collapse navbar-collapse" id="navbarNav">
-			    <ul className="navbar-nav">
-			      {menuItems}
-			    </ul>
-			  </div>
+        	<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<NavLink to="/" className="navbar-brand">{this.props.title}</NavLink>
+				<button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+					{menuItems}
+					</ul>
+				</div>
 			</nav>
         );
     }
