5e8a1f2dd0cba8f09c031b20512dfbab13b7a1cd
Christos Nasikas <xristosnasikas>
2017-09-06 18:29:11 +0300

    Show encrypted data

--- src/components/Data.js
+++ src/components/Data.js
@@ -11,7 +11,15 @@ export default class Data extends React.Component {
           <p className='card-text'>You can store one hash pointer per account</p>
         </div>
         <ul className='list-group list-group-flush'>
-          <li className='list-group-item'>Data: {this.props.data.hash_pointer}</li>
+          <li className='list-group-item'>Data: {this.props.data.ciphertext.ct}</li>
+          <li className='list-group-item'>IV: {this.props.data.ciphertext.iv}</li>
+          <li className='list-group-item'>V: {this.props.data.ciphertext.v}</li>
+          <li className='list-group-item'>Iterations: {this.props.data.ciphertext.iter}</li>
+          <li className='list-group-item'>KS: {this.props.data.ciphertext.ks}</li>
+          <li className='list-group-item'>TS: {this.props.data.ciphertext.ts}</li>
+          <li className='list-group-item'>Mode: {this.props.data.ciphertext.mode}</li>
+          <li className='list-group-item'>Adata: {this.props.data.ciphertext.adata}</li>
+          <li className='list-group-item'>Cipher: {this.props.data.ciphertext.cipher}</li>
         </ul>
         <div className='card-footer text-muted'>
                     Owner: {this.props.user_addr}
