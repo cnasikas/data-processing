f1a521eeff1e551eefb971f840a501203f6a6c41
Christos Nasikas <xristosnasikas@gmail.com>
2017-12-20 14:35:36 +0200

    Add error messages

--- errors/cryptoErrors.js
+++ errors/cryptoErrors.js
@@ -1,9 +1,13 @@
 const emptyKey = new Error('Key buffer cannot be empty.')
-const keysNoProvided = new Error('Please set your symmetric and hmac key.')
+const keysNoEnvSet = new Error('Please set your symmetric and hmac key.')
+const keysNoProvided = new Error('Symmetric and HMAC key should be provided.')
 const wrongKeyLength = new Error('Key is shorter than the expected size')
+const diffKeys = new Error('key and hmac Key MUST be different!')
 
 export default {
   emptyKey,
   wrongKeyLength,
-  keysNoProvided
+  keysNoProvided,
+  keysNoEnvSet,
+  diffKeys
 }
