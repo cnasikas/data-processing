16cb41b4819dee2eef76f7de13b7e79206124da3
Christos Nasikas <xristosnasikas@gmail.com>
2017-12-26 13:57:00 +0200

    Make provideUrl dynamic on nodes

--- backends/EthereumNode.js
+++ backends/EthereumNode.js
@@ -1,5 +1,4 @@
 import Web3 from 'web3'
-import truffle from '../truffle.js'
 
 /* Should refactor to support prmises when web3@1.0.0 is stable and truffle
 * supports it.
@@ -11,9 +10,9 @@ import truffle from '../truffle.js'
 */
 
 export default class EthereumNode {
-  constructor () {
+  constructor (providerURL) {
     this.web3 = new Web3()
-    this.providerURL = 'http://' + truffle.networks.development.host + ':' + truffle.networks.development.port
+    this.providerURL = providerURL
   }
 
   isConnected () {
--- services/Node.js
+++ services/Node.js
@@ -1,4 +1,5 @@
 import config from '../config.json'
+import truffle from '../truffle.js'
 
 function createNode () {
   const backends = {
@@ -8,8 +9,9 @@ function createNode () {
 
   const nodePath = backends[config.backend]
   const NodeClass = require('../backends/' + nodePath).default
+  const providerURL = 'http://' + truffle.networks.development.host + ':' + truffle.networks.development.port
 
-  return new NodeClass()
+  return new NodeClass(providerURL)
 }
 
 export default createNode()
