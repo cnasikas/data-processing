979c61d58d3100aa0f4179f0ef84a09981e92c30
Christos Nasikas <xristosnasikas@gmail.com>
2018-01-03 14:37:00 +0200

    Link to data details

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Data extends React.Component {
   render () {
     return (
       <article className='list-group-item mb-3 data'>
-        <div className='d-flex w-100 justify-content-between'>
-          <h5 className='mb-1'>Data #{this.props.index}</h5>
-          <small>{this.props.date}</small>
-        </div>
-        <p className='mb-1'><b>At:</b> {this.props.hash_ptr}</p>
-        <small><b>By:</b> {this.props.account}</small>
+        <Link to={'datastore/' + this.props._id}>
+          <div className='d-flex w-100 justify-content-between'>
+            <h5 className='mb-1'>Data #{this.props.index}</h5>
+            <small>{this.props.date}</small>
+          </div>
+          <p className='mb-1'><b>At:</b> {this.props.hash_ptr}</p>
+          <small><b>By:</b> {this.props.account}</small>
+        </Link>
       </article>
     )
   }
