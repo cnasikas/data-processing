1d6540199dc9b1e1cd78449ff24e2039b3a98d99
Christos Nasikas <xristosnasikas@gmail.com>
2018-01-31 18:55:22 +0200

    Change data address to byte32

--- contracts/Requests.sol
+++ contracts/Requests.sol
@@ -1,33 +1,34 @@
 pragma solidity ^0.4.11;
 
+
 contract Requests {
 
     address public owner;
 
     struct Request {
-      address addr;
-      bool proof;
+        bytes32 addr;
+        bool proof;
     }
 
     mapping(address => Request) public requests;
 
-    event RequestProcess(address indexed reqAddr, address indexed dataAddr);
+    event RequestProcess(address indexed reqAddr, bytes32 dataAddr);
 
-    function Requests () {
-  		owner = msg.sender;
-  	}
-
-    function requestForProcess(address reqAddr, address dataAddr) {
-
-      requests[reqAddr] = Request({
-        addr: dataAddr,
-        proof: false
-      });
-
-      RequestProcess(reqAddr, dataAddr);
+    function Requests () public {
+        owner = msg.sender;
     }
 
-    function getRequestDataAddr(address reqAddr) constant returns (address) {
-          return requests[reqAddr].addr;
+    function requestForProcess(address reqAddr, bytes32 dataAddr) public {
+
+        requests[reqAddr] = Request({
+            addr: dataAddr,
+            proof: false
+        });
+
+        RequestProcess(reqAddr, dataAddr);
+    }
+
+    function getRequestDataAddr(address reqAddr) public constant returns (bytes32) {
+        return requests[reqAddr].addr;
     }
 }
