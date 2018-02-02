fa34ea4483961efe981d0c57d29e55b6dd2450a2
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-02 14:29:48 +0200

    Change app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
   constructor (props) {
     	super(props)
     	this.state = {
-    		title: 'MPC',
+    		title: 'Data Sharing',
     		menuItems: [
           {id: 'datastore', title: 'Datastore', url: '/datastore'},
           {id: 'requests', title: 'Requests', url: '/requests'},
