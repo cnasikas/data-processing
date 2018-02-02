754f17198a771e1dcb6bf13e64cbf57f591b61cb
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-02 14:41:45 +0200

    Make request linkable to request details

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Badge from './Badge.js'
 
 export default class Request extends React.Component {
@@ -13,18 +14,20 @@ export default class Request extends React.Component {
     let prMsg = proof ? 'Proof' : 'No proof'
 
     return (
-      <article className='list-group-item mb-3'>
-        <div className='d-flex w-100 justify-content-between'>
-          <h5 className='mb-1'>Request #{this.props.id + 1}
-            <Badge type={prcType} msg={prcMsg} />
-            <Badge type={prClass} msg={prMsg} />
-          </h5>
-          <small>{this.props.date}</small>
-        </div>
-        <p className='mb-1'>
-          Data Holder: {this.props.data.data_addr}
-        </p>
-        <small>Request Address: {this.props.user_addr}</small>
+      <article className='list-group-item mb-3 request'>
+        <Link to={'/requests/' + this.props._id}>
+          <div className='d-flex w-100 justify-content-between'>
+            <h5 className='mb-1'>Request #{this.props.id + 1}
+              <Badge type={prcType} msg={prcMsg} />
+              <Badge type={prClass} msg={prMsg} />
+            </h5>
+            <small>{this.props.date}</small>
+          </div>
+          <p className='mb-1'>
+            Data: {this.props.data}
+          </p>
+          <small>Request Address: {this.props.account}</small>
+        </Link>
       </article>
     )
   }
