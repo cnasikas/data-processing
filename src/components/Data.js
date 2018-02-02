54af53223af0748b2f4315c84da2efc3b6925dca
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-02 14:34:11 +0200

    Fix datastore url

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,7 +5,7 @@ export default class Data extends React.Component {
   render () {
     return (
       <article className='list-group-item mb-3 data'>
-        <Link to={'datastore/' + this.props._id}>
+        <Link to={'/datastore/' + this.props._id}>
           <div className='d-flex w-100 justify-content-between'>
             <h5 className='mb-1'>Data #{this.props.index}</h5>
             <small>{this.props.date}</small>
