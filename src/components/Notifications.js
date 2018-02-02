7b4ab4a90ab6f2b15c8a8c2d21118a8ee8f7c680
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-02 14:36:10 +0200

    Fix linting warnings

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { bindActionCreators } from "redux";
+import React from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { removeNotification } from "../actions/ActionCreators";
+import { removeNotification } from '../actions/ActionCreators'
 import Notification from './Notification'
 
 class Notifications extends React.Component {
-
-    render() {
-        let notifications = this.props.notifications.map((notif, index) => 
-        	<Notification key={index} id={index} {...notif} onDismissClick={() => this.props.actions.removeNotification(index)}></Notification>
+  render () {
+    let notifications = this.props.notifications.map((notif, index) =>
+      <Notification key={index} id={index} {...notif} onDismissClick={() => this.props.actions.removeNotification(index)} />
         )
 
-        return(
-            <div className="notifications">{notifications}</div>
-        );
-    }
+    return (
+      <div className='notifications'>{notifications}</div>
+    )
+  }
 }
-
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({ removeNotification }, dispatch)
-});
+})
 
 const mapStateToProps = state => ({
   notifications: state.notifications
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Notifications);
+export default connect(mapStateToProps, mapDispatchToProps)(Notifications)
