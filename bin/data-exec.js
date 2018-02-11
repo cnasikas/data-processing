e99b7c0b3491561ef8b61ddfe506ab14ab7ce69f
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-11 19:37:52 +0200

    Generate dummy file

--- bin/data-exec.js
+++ bin/data-exec.js
@@ -24,6 +24,7 @@ program
   .description('data-exec is a js node app runner that lets you run specific commands for data sharing app')
   .option('-g, --generate-keys', 'Generate an assymetric key pair')
   .option('-s, --generate-key', 'Generate a symetric key')
+  .option('-d, --dummy-file <file>', 'Generate a big dummy file')
   .on('--help', () => {
     console.log(`\
     Examples:
@@ -58,3 +59,18 @@ if (program.generateKey) {
     Key: ${key}
     `)
 }
+
+if (program.dummyFile) {
+  const file = fs.createWriteStream(program.dummyFile)
+
+  for (let i = 0; i <= 1e6; i++) {
+    file.write(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n`)
+  }
+
+  file.end()
+}
+
