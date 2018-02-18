4b6af4fbc4fbc7c8acf05b99639fe739de25dac6
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-18 19:06:02 +0200

    Add more evaluation tests

--- lib/blockchain/contracts/Evaluation.sol
+++ lib/blockchain/contracts/Evaluation.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.4.11;
 
 contract Evaluation {
     bytes public data;
+    bytes public hash;
+
+    event StoreLog(bytes _data);
 
     function storeData(bytes _data) public {
         data = _data;
+    }
+
+    function storeHash(bytes _hash) public {
+        hash = _hash;
+    }
+
+    function storeEvent(bytes _data) public {
+        StoreLog(_data);
     }
 }
