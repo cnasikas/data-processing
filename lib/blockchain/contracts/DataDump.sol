b6696e67c3533a5659a8998046a223257430a8d5
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-22 11:55:58 +0200

    Add DataDump smart contract

--- lib/blockchain/contracts/DataDump.sol
+++ lib/blockchain/contracts/DataDump.sol
@@ -1,16 +1,11 @@
 pragma solidity ^0.4.11;
 
+
 contract DataDump {
 
-    address public owner;
-
-    event DataDumped(address indexed recipient, string dataHash);
-
-    function DataDump () {
-  		owner = msg.sender;
-  	}
-
-    function postData(address recipient, string hash) {
-        DataDumped(recipient, hash);
+    function postData(bytes hash) public {
+        DataDumped(hash);
     }
+
+    event DataDumped(bytes dataHash);
 }
