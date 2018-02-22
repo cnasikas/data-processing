65b90b02cd82ad3bd8cfbdd4ad92803a58ac99f2
Christos Nasikas <xristosnasikas@gmail.com>
2018-02-22 11:52:07 +0200

    Add installation file

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+yarn install
+cd lib/blockchain
+yarn install
+yarn link
+
+cd ../sjcl-all
+yarn link
+
+cd ../total-crypto
+yarn install
+yarn link
+yarn link "sjcl-all"
+
+cd ../../bin
+yarn install
+yarn link "sjcl-all"
+yarn link "blockchain"
+yarn link "total-crypto"
+
+cd ../api
+yarn install
+yarn link "blockchain"
+yarn link "total-crypto"
+
+cd ../dapp
+yarn install
+
+cd ../processor
+yarn install
+yarn link "blockchain"
+yarn link "total-crypto"
