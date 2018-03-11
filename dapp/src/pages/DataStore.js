367868f8a6519c35a6220342c1c19a21f8cce7e8
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-12 00:35:49 +0200

    Change datastore page to HOC

--- dapp/src/pages/DataStore.js
+++ dapp/src/pages/DataStore.js
@@ -1,44 +1,16 @@
 import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import moment from 'moment'
-
+import withList from '../components/ListHOC'
 import { getDataStore } from '../actions/ActionCreators'
 import Data from '../components/Data.js'
-import AddResourceBtn from '../components/AddResourceBtn.js'
 
 import '../css/DataStore.css'
 
-class DataStore extends React.Component {
-  componentDidMount () {
-    this.props.actions.getDataStore().catch(e => console.log(e))
-  }
+const DataList = withList(Data, 'datastore', {getList: getDataStore}, 'Add dataset')
 
+export default class DataStore extends React.Component {
   render () {
-    let datastore = ''
-    if (this.props.datastore.length > 0) {
-      datastore = this.props.datastore.map((data, index) => {
-        let date = !isNaN(new Date(data.created_at)) ? moment(new Date(data.created_at)).format('DD/MM/YYYY') : 'No date provided'
-        return <Data {...data} key={data._id} date={date} index={index + 1} />
-      })
-    }
-
     return (
-
-      <section id='datastore' className='list-group'>
-        {datastore}
-        <AddResourceBtn to='/datastore/add' text='Add Data' />
-      </section>
+      <DataList />
     )
   }
 }
-
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({ getDataStore }, dispatch)
-})
-
-const mapStateToProps = state => ({
-  datastore: state.datastore
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(DataStore)
