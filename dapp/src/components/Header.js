665a44bd317603d3bcdbf4582479f40e291d8c1f
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-12 00:41:32 +0200

    Add processors page link to menu

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -2,24 +2,24 @@ import React from 'react'
 import AppNav from './Nav'
 
 export default class Header extends React.Component {
-
   constructor (props) {
-    	super(props)
-    	this.state = {
-    		title: 'Data Sharing',
-    		menuItems: [
-          {id: 'datastore', title: 'Datastore', url: '/datastore'},
-          {id: 'requests', title: 'Requests', url: '/requests'},
-          {id: 'account', title: 'Account', url: '/account'}
-    ]
-    	}
-  	}
+    super(props)
+    this.state = {
+      title: 'Data Sharing',
+      menuItems: [
+        {id: 'datastore', title: 'Datastore', url: '/datastore'},
+        {id: 'requests', title: 'Requests', url: '/requests'},
+        {id: 'processors', title: 'Processors', url: '/processors'},
+        {id: 'account', title: 'Account', url: '/account'}
+      ]
+    }
+  }
 
   render () {
-    	return (
-      		<header>
+    return (
+      <header>
         <AppNav title={this.state.title} menuItems={this.state.menuItems} />
-      		</header>
-    	)
-  	}
+      </header>
+    )
+  }
 }
