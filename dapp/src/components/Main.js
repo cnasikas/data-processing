6124d01dd0902d8b16c959352f57f5e770f970c2
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-12 00:41:47 +0200

    Route processor urls

--- dapp/src/components/Main.js
+++ dapp/src/components/Main.js
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../pages/Home.js'
 import DataStore from '../pages/DataStore.js'
 import Requests from '../pages/Requests.js'
+import Processors from '../pages/Processors.js'
 import Accounts from '../pages/Accounts.js'
 import Login from './Login.js'
 import AddData from '../pages/AddData.js'
 import AddRequest from '../pages/AddRequest.js'
+import AddProcessor from '../pages/AddProcessor.js'
 import Notifications from '../components/Notifications'
 import DataDetails from '../components/DataDetails'
 import RequestDetails from '../components/RequestDetails'
@@ -25,6 +27,8 @@ export default class Main extends React.Component {
           <Route exact path='/requests' component={Requests} />
           <Route exact path='/requests/add' component={AddRequest} />
           <Route exact path='/requests/:id' component={RequestDetails} />
+          <Route exact path='/processors' component={Processors} />
+          <Route exact path='/processors/add' component={AddProcessor} />
           <Route exact path='/account' component={Accounts} />
           <Route path='/login' component={Login} />
         </Switch>
