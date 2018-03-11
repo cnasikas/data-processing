96062bffa34b821654fdcba692e882fdb7a14cd5
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-12 00:29:13 +0200

    Add dropdown menu to nav

--- dapp/src/components/Nav.js
+++ dapp/src/components/Nav.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default class Nav extends React.Component {
-
   render () {
-	    const menuItems = this.props.menuItems.map((item) =>
-  <li className='nav-item' key={item.id}><NavLink className='nav-link' to={item.url}>{item.title}</NavLink></li>
-		)
+    const menuItems = this.props.menuItems.map((item) =>
+      <li className='nav-item' key={item.id}><NavLink className='nav-link' to={item.url}>{item.title}</NavLink></li>
+    )
 
     return (
 
@@ -18,6 +17,16 @@ export default class Nav extends React.Component {
         <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav'>
             {menuItems}
+            <li className='nav-item dropdown'>
+              <a className='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                Register
+              </a>
+              <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
+                <NavLink className='dropdown-item' to='/datastore/add'>Data set</NavLink>
+                <NavLink className='dropdown-item' to='/requests/add'>Request</NavLink>
+                <NavLink className='dropdown-item' to='/processors/add'>Processor</NavLink>
+              </div>
+            </li>
           </ul>
         </div>
       </nav>
