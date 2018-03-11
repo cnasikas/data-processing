e33469a45f8da29cedd030364f0f4c545e2daaf2
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-12 00:33:22 +0200

    Init process service

--- processor/server.js
+++ processor/server.js
@@ -1,8 +1,10 @@
 import bootstrap from './services/Bootstrap.js'
+import ProcessService from './services/Process.js'
 
 bootstrap()
 .then((blockchain) => {
   console.log('Bootstrap normally executed')
+  const worker = new ProcessService(blockchain, blockchain.listener)
 })
 .catch((err) => {
   console.error('Server error!')
