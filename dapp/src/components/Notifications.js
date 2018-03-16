1a5e1f888987213e2f290037173590ce28c18768
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-16 19:55:22 +0200

    Unique key per notification

--- dapp/src/components/Notifications.js
+++ dapp/src/components/Notifications.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import shortid from 'shortid'
 
 import { removeNotification } from '../actions/ActionCreators'
 import Notification from './Notification'
 
 class Notifications extends React.Component {
+  generateKey (pre) {
+    return shortid.generate()
+  }
+
   render () {
+    if (!this.props.notifications || this.props.notifications.length === 0) {
+      return null
+    }
+
     let notifications = this.props.notifications.map((notif, index) =>
-      <Notification key={index} id={index} {...notif} onDismissClick={() => this.props.actions.removeNotification(index)} />
-        )
+      <Notification key={this.generateKey(index)} {...notif} onDismissClick={() => this.props.actions.removeNotification(index)} />
+    )
 
     return (
       <div className='notifications'>{notifications}</div>
