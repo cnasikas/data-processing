dbc560f0609dcac02fcdeb2ae72144455915a9ec
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-28 15:11:34 +0300

    Refactor store

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -2,52 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import axios from 'axios'
-import axiosMiddleware from 'redux-axios-middleware'
-import { createLogger } from 'redux-logger'
 
-import './index.css'
+import store from './store'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import reducers from './reducers/'
-
-import {
-  notificationActions
-} from './actions'
-
-const addNotification = notificationActions.addNotification
-
-const middlewareConfig = {
-  returnRejectedPromiseOnError: true,
-  interceptors: {
-    response: [
-      {
-        error: ({getState, dispatch, getSourceAction}, error) => {
-          let errorResponse = error.response ? error.response.data.error : error.message
-          let msg = error.name + ': ' + errorResponse
-          dispatch(addNotification({type: 'error', message: msg, class: 'danger'}))
-          return Promise.reject(error)
-        }
-      }
-    ]
-  }
-}
-
-const client = axios.create({
-  baseURL: 'http://localhost:3001/api',
-  responseType: 'json'
-})
-
-const loggerMiddleware = createLogger()
-
-let store = createStore(
-  reducers,
-  applyMiddleware(
-    loggerMiddleware,
-    axiosMiddleware(client, middlewareConfig)
-  )
-)
+import './index.css'
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ dapp/src/store.js
@@ -0,0 +1,46 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import axios from 'axios'
+import axiosMiddleware from 'redux-axios-middleware'
+import reducers from './reducers/'
+
+import {
+  notificationActions
+} from './actions'
+
+const addNotification = notificationActions.addNotification
+
+const client = axios.create({
+  baseURL: 'http://localhost:3001/api',
+  responseType: 'json'
+})
+
+const loggerMiddleware = createLogger()
+
+const middlewareConfig = {
+  returnRejectedPromiseOnError: true,
+  interceptors: {
+    response: [
+      {
+        error: ({getState, dispatch, getSourceAction}, error) => {
+          let errorResponse = error.response ? error.response.data.error : error.message
+          let msg = error.name + ': ' + errorResponse
+          dispatch(addNotification({type: 'error', message: msg, class: 'danger'}))
+          return Promise.reject(error)
+        }
+      }
+    ]
+  }
+}
+
+const store = createStore(
+  reducers,
+  applyMiddleware(
+    loggerMiddleware,
+    thunkMiddleware,
+    axiosMiddleware(client, middlewareConfig)
+  )
+)
+
+export default store
