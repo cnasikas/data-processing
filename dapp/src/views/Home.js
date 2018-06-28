027dbce71e4ba84b693d1cf6057880265a1ae724
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-28 16:53:01 +0300

    Remove contract types from home page

--- dapp/src/views/Home.js
+++ dapp/src/views/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import ContractTypes from '../components/ContractTypes'
 
 export default class Home extends React.Component {
   render () {
     return (
-      <ContractTypes />
+      <div />
     )
   }
 }
