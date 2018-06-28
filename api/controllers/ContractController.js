221d432f12892a3f9e4fc90e20ae17b9bf73e3a4
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-28 16:53:40 +0300

    Return contract abi

--- api/controllers/ContractController.js
+++ api/controllers/ContractController.js
@@ -1,4 +1,5 @@
 import BaseController from './BaseController'
+import Datastore from '../../lib/blockchain/build/contracts/Datastore.json'
 
 export default class ContractController extends BaseController {
   constructor () {
@@ -6,15 +7,6 @@ export default class ContractController extends BaseController {
   }
 
   list (req, res) {
-    let types = []
-
-    let contracts = req.app.blockchain.contracts
-
-    for (let key of Object.keys(contracts)) {
-      let temp = {id: contracts[key].id, title: contracts[key].title, desc: contracts[key].desc}
-      types.push(temp)
-    }
-
-    res.json({types: types})
+    res.json({datastore: Datastore})
   }
 }
