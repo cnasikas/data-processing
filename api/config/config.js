df3d029e8a6baaf1bebc19be2142ce4e4b3e2b14
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-29 13:28:44 +0300

    Init sequelize

---
+++ api/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+    logging: false
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+}
