cea7f3aded8781ba42f0421d42e39c40f98df54b
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-29 15:30:40 +0300

    Update address model associations

--- api/models/address.js
+++ api/models/address.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
   Address.associate = function (models) {
     // associations can be defined here
     Address.belongsTo(models.User, {foreignKey: 'user_id', targetKey: 'id'});
+    Address.hasMany(models.Dataset, {foreignKey: 'address_id', targetKey: 'id'});
+    Address.hasMany(models.Request, {foreignKey: 'address_id', targetKey: 'id'});
+    Address.hasMany(models.Processor, {foreignKey: 'address_id', targetKey: 'id'});
+    Address.hasMany(models.Controller, {foreignKey: 'address_id', targetKey: 'id'});
   }
   return Address
 }
