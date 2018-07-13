66c6757b882be0738428ce954ac1d0e2da7d8729
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-13 14:51:45 +0300

    Add ropsten network

--- blockchain/truffle.js
+++ blockchain/truffle.js
@@ -5,6 +5,11 @@ module.exports = {
       port: 7545,
       gas: 100000000,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      host: '127.0.0.1',
+      port: '8545',
+      network_id: 3
     }
   }
 }
