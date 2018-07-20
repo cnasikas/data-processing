245ef83eb480d23f0684a02de994171322ee79ec
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-20 17:35:00 +0300

    [Processor]: Show start message

--- processor/server.js
+++ processor/server.js
@@ -8,6 +8,8 @@ const ledger = blockchain()
 const node = new ledger.NodeClass(PROVIDER)
 const eventListener = new ledger.Listener(node.contractInstance)
 
+console.log('[*] Processor node started')
+
 const register = async () => {
   await eventListener.registerToEvent('Process')
 
