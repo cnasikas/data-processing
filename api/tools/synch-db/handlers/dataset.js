1627a523dffd53f4afda826381e57d5221bd3b70
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-20 17:35:25 +0300

    [API]: Refactor dataset handler

--- api/tools/synch-db/handlers/dataset.js
+++ api/tools/synch-db/handlers/dataset.js
@@ -1,11 +1,10 @@
-const Sequelize = require('sequelize')
-const {Dataset, Address} = require('../../../models')
+const {saveWithAddress} = require('./save')
+const {Dataset} = require('../../../models')
 
-const ETH_TO_WEI = 1000000000000000000
+// const ETH_TO_WEI = 1000000000000000000
 
 const handleDataset = async (res) => {
-  console.log(res)
-  const datasetObj = {
+  const obj = {
     tx_id: res.transactionHash,
     hash: res.args.hash,
     name: res.args.name,
@@ -15,44 +14,11 @@ const handleDataset = async (res) => {
     status: 'confirmed'
   }
 
-  let addr = await Address.findOrCreate({
-    where: {
-      hash: res.args.controller
-    },
-    defaults: {
-      hash: res.args.controller,
-      user_id: 1 // anonymous user
-    }
-  })
-
-  const addrId = addr[0].dataValues.id // sequelize returs an array
-
-  datasetObj.address_id = addrId
-
-  const dataset = await Dataset.findOne({
-    where: {
-      tx_id: datasetObj.tx_id
-    }
-  })
-
-  if (dataset == null) {
-    const newDataset = await Dataset.create(...datasetObj)
-
-    console.log('Dataset ' + newDataset.id + ' saved to database')
-  } else {
-    const [affectedRows] = await Dataset.update({
-      status: 'confirmed'
-    }, {
-      where: {
-        tx_id: datasetObj.tx_id
-      }
-    })
-
-    if (affectedRows > 0) {
-      console.log('Dataset ' + dataset.id + ' status updated to CONFIRMED')
-    } else {
-      console.log('[!] Dataset with ID ' + dataset.id + ' failed to update')
-    }
+  try {
+    await saveWithAddress(Dataset, {...obj}, res.args._controllerAddress)
+  } catch (err) {
+    console.log(err)
+    throw err
   }
 }
 
