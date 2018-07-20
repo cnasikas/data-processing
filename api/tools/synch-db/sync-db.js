703e523212861ce2bd20549e31b74f8431c21ca0
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-20 18:28:19 +0300

    [API: SYNCH]: Synch dataset

--- api/tools/synch-db/sync-db.js
+++ api/tools/synch-db/sync-db.js
@@ -1,6 +1,6 @@
 (async () => {
   const blockchain = require('blockchain')
-  const {handleEntity} = require('./handlers')
+  const {handleEntity, handleDataset} = require('./handlers')
   const {Processor, Controller} = require('../../models')
 
   const START_BLOCK = 1
@@ -36,17 +36,14 @@
     }
   })
 
-  // instance.NewDataSet({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
-  //   if (!err) {
-  //     for (let datasetEvent of res) {
-  //       console.log(datasetEvent.args)
-  //       console.log('hash:' + node.fromBytes(datasetEvent.args.hash))
-  //       console.log('name:' + node.fromBytes(datasetEvent.args.name))
-  //       console.log('location:' + node.fromBytes(datasetEvent.args.location))
-  //       console.log('category:' + node.fromBytes(datasetEvent.args.category))
-  //       console.log('metaHash:' + node.fromBytes(datasetEvent.args.metaHash))
-  //       await handleDataset(datasetEvent)
-  //     }
-  //   }
-  // })
+  instance.NewDataSet({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
+    if (!err) {
+      for (let event of res) {
+        event.args.name = node.fromBytes(event.args.name)
+        event.args.category = node.fromBytes(event.args.category)
+        event.args.metaHash = node.fromBytes(event.args.metaHash)
+        await handleDataset(event)
+      }
+    }
+  })
 })()
