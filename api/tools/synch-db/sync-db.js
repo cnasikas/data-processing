0fed5fd80348d048168163b86127cef98f2feac8
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-20 17:34:30 +0300

    [API]: Synch entities

--- api/tools/synch-db/sync-db.js
+++ api/tools/synch-db/sync-db.js
@@ -1,6 +1,7 @@
 (async () => {
   const blockchain = require('blockchain')
-  const {handleDataset} = require('./handlers/dataset')
+  const {handleEntity} = require('./handlers')
+  const {Processor, Controller} = require('../../models')
 
   const START_BLOCK = 1
   const END_BLOCK = 'latest'
@@ -17,11 +18,35 @@
   const contract = node.contractInstance
   const instance = await contract.deployed()
 
-  instance.NewDataSet({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
+  instance.NewProcessor({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
     if (!err) {
-      for (let datasetEvent of res) {
-        handleDataset(datasetEvent)
+      for (let event of res) {
+        event.args.name = node.fromBytes(event.args.name)
+        await handleEntity(Processor, event)
       }
     }
   })
+
+  instance.NewController({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
+    if (!err) {
+      for (let event of res) {
+        event.args.name = node.fromBytes(event.args.name)
+        await handleEntity(Controller, event)
+      }
+    }
+  })
+
+  // instance.NewDataSet({}, {fromBlock: START_BLOCK, toBlock: END_BLOCK}).get(async (err, res) => {
+  //   if (!err) {
+  //     for (let datasetEvent of res) {
+  //       console.log(datasetEvent.args)
+  //       console.log('hash:' + node.fromBytes(datasetEvent.args.hash))
+  //       console.log('name:' + node.fromBytes(datasetEvent.args.name))
+  //       console.log('location:' + node.fromBytes(datasetEvent.args.location))
+  //       console.log('category:' + node.fromBytes(datasetEvent.args.category))
+  //       console.log('metaHash:' + node.fromBytes(datasetEvent.args.metaHash))
+  //       await handleDataset(datasetEvent)
+  //     }
+  //   }
+  // })
 })()
