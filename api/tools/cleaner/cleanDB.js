b8ed81b87a8bbc67627ed93d86c4f4877513e0e7
Christos Nasikas <xristosnasikas@gmail.com>
2018-07-23 18:13:01 +0300

    [API]: Create db cleaner tool

---
+++ api/tools/cleaner/cleanDB.js
@@ -0,0 +1,31 @@
+(async () => {
+  const {Processor, Dataset, Request, Controller} = require('../../models')
+  console.log('[*] Cleaning database...')
+
+  try {
+    await Request.destroy({
+      where: {},
+      truncate: { cascade: true }
+    })
+
+    await Processor.destroy({
+      where: {},
+      truncate: { cascade: true }
+    })
+
+    await Controller.destroy({
+      where: {},
+      truncate: { cascade: true }
+    })
+
+    await Dataset.destroy({
+      where: {},
+      truncate: { cascade: true }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+
+  console.log('[*] All cleaned!')
+  process.exit()
+})()
