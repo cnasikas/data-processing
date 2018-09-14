8a4a9f925a470ead2bc96ee07a7ff028c34446ca
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-14 17:16:45 +0300

    [API]: Download dataset route

--- api/routes/datastore.js
+++ api/routes/datastore.js
@@ -1,4 +1,9 @@
 import {createSimpleRouter} from '../utils/routes'
-const {router} = createSimpleRouter('DataStoreController')
+const {router, controller} = createSimpleRouter('DataStoreController')
+
+router.get(
+  '/:id/get',
+  (req, res, next) => { controller.download(req, res, req.params.id).catch(next) }
+)
 
 export default router
