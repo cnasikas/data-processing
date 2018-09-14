6729550ff7391004a529a7852ea9f168b47fc915
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-14 12:09:36 +0300

    [DAPP]: Create controllers page

---
+++ dapp/src/views/Controllers.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import withList from '../components/ListHOC'
+import Controller from '../components/Controller'
+
+import '../css/Entities.css'
+
+import {
+  controllerActions
+} from '../actions'
+
+const getControllers = controllerActions.getControllers
+
+const Controllers = withList(Controller, 'controllers', {getList: getControllers}, 'Register Controller')
+
+export default class ControllersPage extends React.Component {
+  render () {
+    return (
+      <Controllers />
+    )
+  }
+}
