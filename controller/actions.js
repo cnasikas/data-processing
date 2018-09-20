eeca9f811179143740cd5b1f665e66094a182330
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-20 19:10:38 +0300

    [CONTROLLER]: Fix linting errors

--- controller/actions.js
+++ controller/actions.js
@@ -4,13 +4,13 @@ const crypto = new Crypto()
 
 const encryptKey = (key) => {
   let encKey = crypto.pubEncrypt(key, process.env.SYM_KEY)
-  let {iv, kemtag, ct} = JSON.parse(encKey)
-  return JSON.stringify({iv, kemtag, ct})
+  let { iv, kemtag, ct } = JSON.parse(encKey)
+  return JSON.stringify({ iv, kemtag, ct })
 }
 
 const forwardToProcessor = async (node, data) => {
   try {
-    let {_requestID} = data
+    let { _requestID } = data
     console.log(`[*] Got new request with id: ${_requestID}`)
     // const bytes = node.toBytes(hex)
     let account = node.getDefaultAccount()
