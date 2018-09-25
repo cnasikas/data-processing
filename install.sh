d010bcf927273dce36ecba16a9eb5d4fc03950c1
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-25 14:56:38 +0300

    [MAIN]: Seperate installation from linking

--- install.sh
+++ install.sh
@@ -9,71 +9,33 @@ yarn install
 
 cd lib/util
 yarn install
-yarn link
 
 cd ../dataset-manager
 yarn install
-yarn link
 
 cd ../blockchain
 yarn install
-yarn link
-yarn link "data-market-utils"
 
 cd ../db
 yarn install
-yarn link
-yarn link "blockchain"
-
-cd ../sjcl-all
-yarn link
 
 cd ../total-crypto
 yarn install
-yarn link
-yarn link "sjcl-all"
-yarn link "data-market-utils"
 
 cd ../../cli
 yarn install
-yarn link "sjcl-all"
-yarn link "blockchain"
-yarn link "total-crypto"
-yarn link "data-market-utils"
-yarn link "data-market-db"
-yarn link "dataset-manager"
 
 cd ../api
 yarn install
-yarn link "blockchain"
-yarn link "total-crypto"
-yarn link "data-market-utils"
-yarn link "data-market-db"
-yarn link "dataset-manager"
 
 cd ../dapp
 yarn install
-yarn link "blockchain"
 
 cd ../processor
 yarn install
-yarn link "blockchain"
-yarn link "total-crypto"
-yarn link "data-market-utils"
-yarn link "data-market-db"
-yarn link "dataset-manager"
 
 cd ../controller
 yarn install
-yarn link "blockchain"
-yarn link "total-crypto"
-yarn link "data-market-utils"
-yarn link "data-market-db"
-yarn link "dataset-manager"
 
 cd ../explorer
 yarn install
-yarn link "blockchain"
-yarn link "total-crypto"
-yarn link "data-market-utils"
-yarn link "data-market-db"
---
+++ link_libraries.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+cd lib/util
+yarn link
+
+cd ../dataset-manager
+yarn link
+
+cd ../blockchain
+yarn link
+yarn link "data-market-utils"
+
+cd ../db
+yarn link
+yarn link "blockchain"
+
+cd ../sjcl-all
+yarn link
+
+cd ../total-crypto
+yarn link
+yarn link "sjcl-all"
+yarn link "data-market-utils"
+
+cd ../../cli
+yarn link "sjcl-all"
+yarn link "blockchain"
+yarn link "total-crypto"
+yarn link "data-market-utils"
+yarn link "data-market-db"
+yarn link "dataset-manager"
+
+cd ../api
+yarn link "blockchain"
+yarn link "total-crypto"
+yarn link "data-market-utils"
+yarn link "data-market-db"
+yarn link "dataset-manager"
+
+cd ../dapp
+yarn link "blockchain"
+
+cd ../processor
+yarn link "blockchain"
+yarn link "total-crypto"
+yarn link "data-market-utils"
+yarn link "data-market-db"
+yarn link "dataset-manager"
+
+cd ../controller
+yarn link "blockchain"
+yarn link "total-crypto"
+yarn link "data-market-utils"
+yarn link "data-market-db"
+yarn link "dataset-manager"
+
+cd ../explorer
+yarn link "blockchain"
+yarn link "total-crypto"
+yarn link "data-market-utils"
+yarn link "data-market-db"
