7c021318ee4ea388687a22d492f19987f5b9af5f
Christos Nasikas <xristosnasikas@gmail.com>
2018-10-29 15:06:08 +0200

    [API]: Change welcome message

--- api/server.js
+++ api/server.js
@@ -3,9 +3,6 @@ import express from 'express'
 
 import bootstrap from './services/Bootstrap.js'
 
-/* TODO: CHECK SECURITY */
-/* TODO: Handle errors! */
-
 const app = express()
 
 app.get('/api/health', (_, res) => res.status(204).send())
@@ -13,5 +10,5 @@ app.get('/api/health', (_, res) => res.status(204).send())
 bootstrap(app)
 
 app.listen(process.env.PORT || 3001, function () {
-  console.log(`Started on port ${this.address().port}`)
+  console.log(`[*] Started on port ${this.address().port}`)
 })
