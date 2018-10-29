9db85fa60a806ce613b82aaaca910403545f6680
Christos Nasikas <xristosnasikas@gmail.com>
2018-10-29 21:11:55 +0200

    [EXPLORER]: Listen to events from proof contract

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -11,7 +11,7 @@ const registerEvent = async (eventListener, event) => {
   await eventListener.registerToEvent(event)
 }
 
-const listenToEvents = async (node, eventListener) => {
+const listenToEvents = async (node, contracts) => {
   /*
    NewDataSet
    NewController
@@ -20,44 +20,59 @@ const listenToEvents = async (node, eventListener) => {
    Process
   */
 
-  const events = ['NewProcessor', 'NewController', 'NewDataSet', 'NewRequest']
-
-  for (const event of events) {
-    registerEvent(eventListener, event)
+  for (const contract of contracts) {
+    for (const event of contract.events) {
+      registerEvent(contract.listener, event)
+    }
   }
 
-  eventListener.on('NewProcessor', async (req) => {
+  const dataStoreListener = contracts[0].listener
+  const proofStoreListener = contracts[1].listener
+
+  dataStoreListener.on('NewProcessor', async (req) => {
     req.args.name = node.fromBytes(req.args.name)
     await eventHandlers.handleEntity(models.Processor, req)
   })
 
-  eventListener.on('NewController', async (req) => {
+  dataStoreListener.on('NewController', async (req) => {
     req.args.name = node.fromBytes(req.args.name)
     await eventHandlers.handleEntity(models.Controller, req)
   })
 
-  eventListener.on('NewDataSet', async (req) => {
+  dataStoreListener.on('NewDataSet', async (req) => {
     req.args.name = node.fromBytes(req.args.name)
     req.args.hash = req.args.hash.substring(2)
     req.args.category = node.fromBytes(req.args.category)
     await eventHandlers.handleDataset(req)
   })
 
-  eventListener.on('NewRequest', async (req) => {
+  dataStoreListener.on('NewRequest', async (req) => {
     req.args.algorithmID = node.fromBytes(req.args.algorithmID)
     req.args._dataSetID = req.args._dataSetID.substring(2)
     await eventHandlers.handleRequest(req)
+  })
+
+  proofStoreListener.on('NewProof', async (req) => {
+    await eventHandlers.handleProof(req)
   })
 }
 
 const main = async () => {
   const ledger = blockchain()
   const node = new ledger.NodeClass(PROVIDER)
+  const dataStoreEvents = ['NewProcessor', 'NewController', 'NewDataSet', 'NewRequest']
+  const proofEvents = ['NewProof']
 
   try {
     await node.init()
-    const eventListener = new ledger.Listener(node.contractInstance)
-    await listenToEvents(node, eventListener)
+    const dataStoreListener = new ledger.Listener(node.getDataStore())
+    const proofListener = new ledger.Listener(node.getProofStore())
+    await listenToEvents(node,
+      [
+        { events: dataStoreEvents, listener: dataStoreListener },
+        { events: proofEvents, listener: proofListener }
+      ]
+    )
   } catch (e) {
     console.log(e.message)
     process.exit(1)
