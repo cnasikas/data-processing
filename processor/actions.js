19d05bc66b2eb9dee9c2f7221e658b6ce5829a13
Christos Nasikas <xristosnasikas@gmail.com>
2018-10-31 21:35:49 +0200

    [PROCESSOR]: Refactor actions

--- processor/actions.js
+++ processor/actions.js
@@ -2,6 +2,7 @@ import util from 'util'
 import childProcess from 'child_process'
 import fs from 'fs'
 import path from 'path'
+import _ from 'lodash'
 
 import Crypto from 'total-crypto'
 import dsm from 'dataset-manager'
@@ -28,18 +29,26 @@ const compute = async (filePath, symKey) => {
   console.log(`[*] Done!`)
 }
 
-const processData = async (symKey, location) => {
+const processData = async (processingInfo) => {
   console.log(`[*] Downloading dataset...`)
-  const filePath = await datasetManager.download(location)
+  const filePath = await datasetManager.download(processingInfo.dataset.location)
   console.log(`[*] Done!`)
 
   const iv = '3ec7cf091636fd12d28aeaa5e4d614e6'
   const hmacKey = '191a416b8e6e646e3787e3c5601a91985a8df8fdb1444e9d8862ed68719b8e8e'
 
   console.log(`[*] Decrypting dataset...`)
-  const { absPath } = await crypto.decryptFile(symKey, hmacKey, filePath, iv)
+  const { absPath } = await crypto.decryptFile(processingInfo.dataset.symKey, hmacKey, filePath, iv)
   console.log(`[*] Done! At: ${absPath}`)
-  await compute(absPath, symKey)
+
+  await compute(absPath, processingInfo.dataset.symKey)
+
+  return {
+    dataset: {
+      filePath,
+      absPath
+    }
+  }
 }
 
 const saveSymKey = async (symKey, datasetID, filePath) => {
@@ -48,14 +57,55 @@ const saveSymKey = async (symKey, datasetID, filePath) => {
   await writeFile(file, symKey)
 }
 
-const saveProofToBlockchain = async (node, datasetID, requestID) => {
+const saveProofToBlockchain = async (node, processingInfo) => {
+  console.log(`[*] Saving proof to the blockchain...`)
+  const datasetID = processingInfo.dataset.id.substring(2)
   const proofFile = `${DATASET_FOLDER}/${datasetID}.proof`
 
   /* Proof is small and constant so it is ok to load the file to memory */
   let proof = await readFile(path.join(__dirname, proofFile))
   proof = parseProof(proof)
-  const tx = await node.addProof(requestID, proof)
+  const tx = await node.addProof(processingInfo.request.id, proof)
   console.log(`[*] Proof saved. Tx: ${tx}`)
+}
+
+const getRequestInfo = async (node, processingInfo) => {
+  console.log(`[*] Getting request info...`)
+  const request = await node.getRequestInfo(processingInfo.request.id)
+  let [_dataSetID, algorithmID, pubKey] = request
+
+  console.log(`[*] Done: datasetID: ${_dataSetID}, algorithmID: ${algorithmID}, pubKey: ${pubKey}`)
+  console.log(`[*] Getting dataset info...`)
+  let dataset = await node.getDataSetInfo(_dataSetID)
+  let [name, location] = dataset // name, location, category, metaHash, controller
+  console.log(`[*] Done: name: ${name}, location: ${location}`)
+
+  return {
+    request: {
+      algorithmID,
+      pubKey
+    },
+    dataset: {
+      id: _dataSetID,
+      name,
+      location
+    }
+  }
+}
+
+const handleFileKey = async (processingInfo) => {
+  console.log(`[*] Decrypting symetric key...`)
+  let symKey = decryptKey(processingInfo.fileKey.cipher)
+  console.log(`[*] Done!`)
+
+  console.log(`[*] Saving symetric key...`)
+  await saveSymKey(symKey, processingInfo.dataset.id.substring(2), path.join(__dirname, DATASET_FOLDER))
+  console.log(`[*] Done!`)
+
+  return {
+    dataset: { symKey },
+    fileKey: { plain: symKey }
+  }
 }
 
 const handleProcess = async (node, data) => {
@@ -64,38 +114,31 @@ const handleProcess = async (node, data) => {
 
     if (_processorAddress === node.getDefaultAccount()) {
       console.log(`[*] Got new processing request with id: ${_requestID}`)
+      /* eslint-disable-next-line no-unused-vars */
+      let processingInfo = {
+        request: {
+          id: _requestID
+        },
+        fileKey: {
+          cipher: encryptedKey
+        }
+      }
 
-      // let account = node.getDefaultAccount()
-      console.log(`[*] Getting request info...`)
-      let request = await node.getRequestInfo(_requestID)
-      let [_dataSetID, algorithmID, pubKey] = request
+      const requestInfo = await getRequestInfo(node, processingInfo)
+      processingInfo = _.merge(processingInfo, requestInfo)
 
-      console.log(`[*] Done: datasetID: ${_dataSetID}, algorithmID: ${algorithmID}, pubKey: ${pubKey}`)
-      console.log(`[*] Getting dataset info...`)
-      let dataset = await node.getDataSetInfo(_dataSetID)
-      let [name, location] = dataset // name, location, category, metaHash, controller
-      console.log(`[*] Done: name: ${name}, location: ${location}`)
+      const keyInfo = await handleFileKey(processingInfo)
+      processingInfo = _.merge(processingInfo, keyInfo)
 
-      // let hash = this.crypto.hash([dataset[0], dataset[1], dataset[2], dataset[3]]) // name, location, category, account (controller)
-      // assert.equal(hash, dataset[4], 'Metadata hash not match!!')
-      console.log(`[*] Decrypting symetric key...`)
-      let symKey = decryptKey(encryptedKey)
-      console.log(`[*] Done!`)
+      const processInfo = await processData(processingInfo)
+      processingInfo = _.merge(processingInfo, processInfo)
 
-      console.log(`[*] Saving symetric key...`)
-      await saveSymKey(symKey, _dataSetID.substring(2), path.join(__dirname, DATASET_FOLDER))
-      console.log(`[*] Done!`)
-
-      await processData(symKey, location)
-
-      console.log(`[*] Saving proof to the blockchain...`)
-      await saveProofToBlockchain(node, _dataSetID.substring(2), _requestID)
-      console.log(`[*] Done!`)
+      await saveProofToBlockchain(node, processingInfo)
     } else {
       console.log(`[*] Process request not assigned to me!`)
     }
-  } catch (e) {
-    throw new Error(e)
+  } catch (err) {
+    throw err
   }
 }
 
