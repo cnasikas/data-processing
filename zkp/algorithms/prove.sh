1a1c272f594a9211b9bf9963fc061717ab880ff4
Christos Nasikas <xristosnasikas@gmail.com>
2018-11-05 17:49:04 +0200

    [ZKP]: Automate json export

--- zkp/algorithms/prove.sh
+++ zkp/algorithms/prove.sh
@@ -9,4 +9,10 @@ cd ../
 echo "$(date) construct proof" | tee -a $LOG
 SECONDS=0
 ./bin/pepper_prover_$APP prove $APP.pkey $APP.inputs $APP.outputs $APP.proof
+
+cd ./utils
+./export proof ../prover_verifier_shared/$APP.proof
+cd ../
+mv ./utils/proof.json ./prover_verifier_shared/$APP.json
+
 echo "$(date) construct proof done, took $SECONDS seconds" | tee -a $LOG
