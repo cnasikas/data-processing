aa65c9128a26883f95dd1f350fc025afc4f2ff6d
Christos Nasikas <xristosnasikas@gmail.com>
2018-11-08 09:05:26 -0800

    [ZKP]: Generate inputs

--- zkp/algorithms/prove.sh
+++ zkp/algorithms/prove.sh
@@ -1,18 +1,24 @@
 APP=$1
+HASH=$2
 LOG="$APP.log"
 cd "$(dirname "$0")"
 
+cd ../ # zkp/pequin/pepper
+
+./bin/pepper_verifier_$APP gen_input $APP.inputs $HASH
+
+cd ./apps # zkp/pequin/pepper/apps
+
 cp $APP/exo0 ../bin/
-cp $APP/$APP.inputs ../prover_verifier_shared/$APP.inputs
-cd ../
+cd ../ # zkp/pequin/pepper
 
 echo "$(date) construct proof" | tee -a $LOG
 SECONDS=0
 ./bin/pepper_prover_$APP prove $APP.pkey $APP.inputs $APP.outputs $APP.proof
 
-cd ./utils
-./export proof ../prover_verifier_shared/$APP.proof
-cd ../
-mv ./utils/proof.json ./prover_verifier_shared/$APP.json
+cd ../../utils # zkp/utils
+./export proof ../pequin/pepper/prover_verifier_shared/$APP.proof
+cd ../ # zkp
+mv ./utils/proof.json ./pequin/pepper/prover_verifier_shared/$APP.json
 
 echo "$(date) construct proof done, took $SECONDS seconds" | tee -a $LOG
